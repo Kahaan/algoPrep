6a9bb023017b81780f03915d901abdd4cc6afd82
Kahaan <kahaan.r.patel@gmail.com>
2018-09-10 12:40:42 -0700

    Algo prep

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,8 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+
 function maxChar(str) {
-  
+  hsh = {}
+  for (let el of str){
+    hsh[el] = hsh[el] + 1 || 1
+  }
+
+  maxVal = 0
+  maxChar = ''
+
+  for(let ch in hsh){
+    if (maxVal < hsh[ch]){
+      maxVal = hsh[ch]
+      maxChar = ch
+    }
+  }
+
+  return maxChar
 }
+
 
 module.exports = maxChar;
