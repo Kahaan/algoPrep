d8ee14e27896300349f7425edc8172cebc119e43
Kahaan <kahaan.r.patel@gmail.com>
2018-09-18 11:02:58 -0700

    algo prep

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -20,6 +20,68 @@ class LinkedList {
     this.head = node
   }
 
+  size(){
+    let size = 0
+    let curNode = this.head
+    while (curNode){
+      size += 1
+      curNode = curNode.next
+    }
+    return size
+  }
+
+  getFirst(){
+    return this.head
+  }
+
+  getLast(){
+    let curNode = this.head
+    while(curNode){
+      if (curNode.next === null) {
+        return curNode
+      }
+      curNode = curNode.next
+    }
+  }
+
+  clear(){
+    this.head = null
+  }
+
+  removeFirst(){
+    this.head = this.head.next
+  }
+
+  removeLast(){
+    // if there are no nodes
+    if (this.head === null) return
+    let prevNode = this.head
+    let node = this.head.next
+    // if there is only one node
+    if (!this.head.next) {
+      this.head = null
+      return
+    }
+
+    while(node.next){
+      prevNode = node
+      node = node.next
+    }
+    prevNode.next = null
+  }
+
+  insertLast(Data){
+    newNode = new Node(Data)
+    curNode = this.head
+    while (curNode) {
+      if (curNode.next === null){
+        curNode.next = newNode
+        return
+      }
+      curNode = curNode.next
+    }
+  }
+
 }
 
 module.exports = { Node, LinkedList };
